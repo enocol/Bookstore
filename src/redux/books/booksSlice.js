89d3a7aeccd881891933700becbad6f52a71ddf3
Enoh Collins <eno.col@gmail.com>
2023-10-06 13:31:33 +0100

    Edit to remove comments

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,7 +1,8 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const apiEndPoint = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/9g8gWcp1UOlVIcNnZwEx/books';
+const apiEndPoint =
+  'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/9g8gWcp1UOlVIcNnZwEx/books';
 
 export const postData = createAsyncThunk('books/postBooks', async (newData) => {
   const response = await axios.post(apiEndPoint, newData);
@@ -66,10 +67,6 @@ const booksSlice = createSlice({
       .addCase(postData.pending, (state) => {
         state.status = 'loading';
       })
-      // .addCase(postData.fulfilled, (state, action) => {
-      //   state.status = 'succeeded';
-      //   state.books.push(action.payload);
-      // })
 
       .addCase(postData.fulfilled, (state) => ({
         ...state,
@@ -85,3 +82,4 @@ const booksSlice = createSlice({
 export const { bookAdded, bookRemoved } = booksSlice.actions;
 
 export default booksSlice.reducer;
+
