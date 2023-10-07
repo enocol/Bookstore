ecdc403920c86bfcb5a6e60e311de58d9b3e1406
Enoh Collins <eno.col@gmail.com>
2023-10-07 04:37:36 +0100

    Edit Navbar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,18 +4,24 @@ const NavBar = () => (
   <>
     <header className="header">
       <nav className="navbar">
-        <h1>Book Store</h1>
+        <h1>Bookstore CMS</h1>
         <ul className="navlinks">
-          <li>
+          <li className="link-b">
             <Link to="/">Books</Link>
           </li>
           <li>
-            <Link to="/catergories">Catergories</Link>
+            <Link to="/catergories" className="link-c">
+              Catergories
+            </Link>
           </li>
         </ul>
       </nav>
+      <div className="profile">
+        <i class="fa-sharp fa-solid fa-user fa-sm"></i>
+      </div>
     </header>
   </>
 );
 
 export default NavBar;
+
